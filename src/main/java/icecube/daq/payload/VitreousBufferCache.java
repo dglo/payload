bf53b39c8dd95dd4d0e6a5ae142f209e32c59e0b
Kael Hanson <kael.hanson@gmail.com>
2007-03-18 16:37:47 -0500

    adding to svn repo

---
+++ src/main/java/icecube/daq/payload/VitreousBufferCache.java
@@ -0,0 +1,91 @@
+package icecube.daq.payload;
+
+import java.nio.ByteBuffer;
+
+/**
+ * This is class which emulates the ByteBufferCache by
+ * simply going back to the heap and asking for buffers
+ * directly from the VM.  It does not cache the buffers
+ * in any way.
+ * @author kael
+ *
+ */
+public class VitreousBufferCache implements IByteBufferCache, ByteBufferCacheMBean
+{
+    private int acquiredBufferCount;
+    private long acquiredBytes;
+    private int returnedBuffers;
+    
+    public VitreousBufferCache()
+    {
+        acquiredBufferCount = 0;
+        acquiredBytes = 0L;
+        returnedBuffers = 0;
+    }
+    
+    public synchronized ByteBuffer acquireBuffer(int iLength)
+    {
+        acquiredBufferCount++;
+        acquiredBytes += iLength;
+        return ByteBuffer.allocate(iLength);
+    }
+
+    public void flush() { }
+
+    public synchronized int getCurrentAquiredBuffers()
+    {
+        return acquiredBufferCount;
+    }
+
+    public synchronized long getCurrentAquiredBytes()
+    {
+        return acquiredBytes;
+    }
+
+    public synchronized int getTotalBuffersAcquired()
+    {
+        return acquiredBufferCount;
+    }
+
+    public synchronized int getTotalBuffersCreated()
+    {
+        return acquiredBufferCount;
+    }
+
+    public synchronized int getTotalBuffersReturned()
+    {
+        return returnedBuffers;
+    }
+
+    public synchronized long getTotalBytesInCache()
+    {
+        return acquiredBytes;
+    }
+
+    public boolean isBalanced()
+    {
+        return false;
+    }
+
+    public void returnBuffer(ByteBuffer tByteBuffer) { }
+
+    public void destinationClosed() { }
+
+    public void receiveByteBuffer(ByteBuffer tBuffer) { }
+
+    public synchronized int getReturnBufferCount()
+    {
+        return returnedBuffers;
+    }
+
+    public int getReturnBufferEntryCount()
+    {
+        return 0;
+    }
+
+    public long getReturnBufferTime()
+    {
+        return 0;
+    }
+
+}
