6c5b406414b77569470deffbd2dcd27fa918ac80
Dave Glowacki <dglo@icecube.wisc.edu>
2007-10-11 13:01:49 -0500

    Issue 1829: Avoid building an extra PayloadEnvelope object for each new payload

--- src/main/java/icecube/daq/payload/MasterPayloadFactory.java
+++ src/main/java/icecube/daq/payload/MasterPayloadFactory.java
@@ -71,24 +71,6 @@ public class MasterPayloadFactory extends PayloadFactory {
 
 
     /**
-     * This method must be implemented specific to the format of the
-     * the input stream to determine when a complete data element is available.
-     * @param iOffset The offset in the ByteBuffer from which to create the payload/spliceable
-     * @param tBuffer ByteBuffer from which to detect a spliceable.
-     *
-     * @return the PayloadEnvelope for this payload
-     *
-     * @exception IOException if there is an error reading the ByteBuffer
-     *                                    to pull out the length of the spliceable.
-     * @exception DataFormatException if there is an error in the format of the payload
-     */
-    private static PayloadEnvelope readPayloadEnvelope(int iOffset, ByteBuffer tBuffer) throws IOException, DataFormatException {
-        PayloadEnvelope tEnvelope = (PayloadEnvelope) PayloadEnvelope.getFromPool();
-        tEnvelope.loadData(iOffset, tBuffer);
-        return tEnvelope;
-    }
-
-    /**
      * Returns an empty Spliceable object representing the current place in the
      * order of Spliceable objects.
      *  Abstract function which must be implemented by the specific
@@ -133,13 +115,16 @@ public class MasterPayloadFactory extends PayloadFactory {
      */
     public Payload createPayload(int iOffset, ByteBuffer tPayloadBuffer, boolean bCreateSeperateBuffers) throws IOException,DataFormatException {
         Payload tPayload = null;
-        //-TODO: put in convenience method to read type only... reduce object creation
-        PayloadEnvelope tEnvelope = readPayloadEnvelope(iOffset, tPayloadBuffer);
-        PayloadFactory tFactory = mtPayloadRegistry.getPayloadFactory(tEnvelope.miPayloadType);
+        int pType =
+            PayloadEnvelope.readPayloadType(iOffset, tPayloadBuffer);
+        PayloadFactory tFactory = mtPayloadRegistry.getPayloadFactory(pType);
         if (tFactory == null) {
-            throw new DataFormatException("Factory associated with payload #" + tEnvelope.miPayloadType + " not found");
-            //return null;
+            throw new DataFormatException("Factory associated with payload #" +
+                                          pType + " not found");
         }
+
+        int pLen =
+            PayloadEnvelope.readPayloadLength(iOffset, tPayloadBuffer);
 
         //-This section is needed to make buffering distinct from PayloadCreation.
         // This allows the system to be tested distinctly from buffering.
@@ -148,14 +133,14 @@ public class MasterPayloadFactory extends PayloadFactory {
             if (mtBufferCache != null) {
                 //-create the new ByteBuffer from the installed buffer cache
                 // and copy the contents into the new ByteBuffer before creation
-                tNewPayloadBuffer = mtBufferCache.acquireBuffer(tEnvelope.miPayloadLen);
+                tNewPayloadBuffer = mtBufferCache.acquireBuffer(pLen);
                 int iOldLimit = tPayloadBuffer.limit();
                 int iOldPos   = tPayloadBuffer.position();
                 tPayloadBuffer.position(iOffset);
-                tPayloadBuffer.limit(iOffset + tEnvelope.miPayloadLen);
+                tPayloadBuffer.limit(iOffset + pLen);
                 //-setup position and limit for copy
                 tNewPayloadBuffer.position(0);
-                tNewPayloadBuffer.limit(tEnvelope.miPayloadLen);
+                tNewPayloadBuffer.limit(pLen);
                 tNewPayloadBuffer.put(tPayloadBuffer);
                 //-reset the position and limit
                 tNewPayloadBuffer.position(0);
@@ -163,11 +148,11 @@ public class MasterPayloadFactory extends PayloadFactory {
                 tPayloadBuffer.limit(iOldLimit);
             } else {
                 //-allocate the seperate buffer
-                byte[] tNewPayloadBufferBytes = new byte[tEnvelope.miPayloadLen];
+                byte[] tNewPayloadBufferBytes = new byte[pLen];
                 //-copy the data from the old buffer to the njew one.
                 int iOldPosition = tPayloadBuffer.position();
                 tPayloadBuffer.position(iOffset);
-                tPayloadBuffer.get(tNewPayloadBufferBytes, 0, tEnvelope.miPayloadLen);
+                tPayloadBuffer.get(tNewPayloadBufferBytes, 0, pLen);
                 tNewPayloadBuffer = ByteBuffer.wrap( tNewPayloadBufferBytes );
                 tPayloadBuffer.position(iOldPosition);
             }
@@ -177,7 +162,6 @@ public class MasterPayloadFactory extends PayloadFactory {
             //-create the payload using the input ByteBuffer
             tPayload = tFactory.createPayload(iOffset, tPayloadBuffer);
         }
-        tEnvelope.recycle();
         return tPayload;
     }
     /**
