89f961319456fae5d71ec253c935e1f40dc40d73
Dave Glowacki <dglo@icecube.wisc.edu>
2008-10-09 06:40:59 -0500

    Issue 2916: Sync implementations of MockDOMID

--- src/test/java/icecube/daq/payload/test/MockDOMID.java
+++ src/test/java/icecube/daq/payload/test/MockDOMID.java
@@ -5,20 +5,62 @@ import icecube.daq.payload.IDOMID;
 public class MockDOMID
     implements IDOMID
 {
-    private long id;
+    private long domId;
 
-    public MockDOMID(long id)
+    public MockDOMID(long domId)
     {
-        this.id = id;
+        this.domId = domId;
+    }
+
+    public int compareTo(Object obj)
+    {
+        if (obj == null) {
+            return 1;
+        } else if (!(obj instanceof IDOMID)) {
+            return getClass().getName().compareTo(obj.getClass().getName());
+        }
+
+        final long val = ((IDOMID) obj).longValue();
+        if (domId < val) {
+            return -1;
+        } else if (domId > val) {
+            return 1;
+        }
+
+        return 0;
     }
 
     public Object deepCopy()
     {
-        throw new Error("Unimplemented");
+        return new MockDOMID(domId);
+    }
+
+    public boolean equals(Object obj)
+    {
+        return compareTo(obj) == 0;
     }
 
     public long longValue()
     {
-        return id;
+        return domId;
+    }
+
+    public int hashCode()
+    {
+        final long modValue = Integer.MAX_VALUE / 256;
+
+        final long topTwo = domId / modValue;
+
+        return (int) (topTwo / modValue) + (int) (topTwo % modValue) +
+            (int) (domId % modValue);
+    }
+
+    public String toString()
+    {
+        String str = Long.toHexString(domId);
+        while (str.length() < 12) {
+            str = "0" + str;
+        }
+        return str;
     }
 }
