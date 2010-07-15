e70edae353d4f4b18c4eb8a852fcd01361bc3337
Dave Glowacki <dglo@icecube.wisc.edu>
2010-07-15 10:14:48 -0500

    Issue 4092: Properly locate getPayloadBacking()

--- src/main/java/icecube/daq/payload/IPayload.java
+++ src/main/java/icecube/daq/payload/IPayload.java
@@ -1,7 +1,7 @@
 /*
  * class: IPayload
  *
- * Version $Id: IPayload.java 4574 2009-08-28 21:32:32Z dglo $
+ * Version $Id: IPayload.java 5096 2010-07-15 20:14:48Z dglo $
  *
  * Date: September 17 2004
  *
@@ -15,12 +15,21 @@ import java.nio.ByteBuffer;
 /**
  * Basic interface defining a trigger primitive payload
  *
- * @version $Id: IPayload.java 4574 2009-08-28 21:32:32Z dglo $
+ * @version $Id: IPayload.java 5096 2010-07-15 20:14:48Z dglo $
  * @author hellwig, dwharton
  */
 public interface IPayload
     extends ICopyable
 {
+    /**
+     * Returns the ByteBuffer which backs this payload
+     * if it has one.
+     * @return the backing of this payload if it has one.
+     *                        this will be null if it is not 'backed'.
+     * NOTE: dbw: this is for Chuck McParland for testing...
+     */
+    ByteBuffer getPayloadBacking();
+
     /**
      * returns the length in bytes of this payload
      * @return payload length
@@ -45,15 +54,6 @@ public interface IPayload
      * @return time
      */
     IUTCTime getPayloadTimeUTC();
-
-    /**
-     * Returns the ByteBuffer which backs this payload
-     * if it has one.
-     * @return the backing of this payload if it has one.
-     *                        this will be null if it is not 'backed'.
-     * NOTE: dbw: this is for Chuck McParland for testing...
-     */
-    ByteBuffer getPayloadBacking();
 
     /**
      * Set the buffer cache for this payload.
