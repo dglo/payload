3a260aa8f0a8213a2b4f654774c116cfe873f2d8
IceCube Intern <intern@icecube.wisc.edu>
2011-04-01 11:02:36 -0500

    message

--- src/test/java/icecube/daq/oldpayload/impl/ByteBufferPayloadDestinationTest.java
+++ src/test/java/icecube/daq/oldpayload/impl/ByteBufferPayloadDestinationTest.java
@@ -1,23 +1,83 @@
-package icecube.daq.payload.impl;
+package icecube.daq.oldpayload;
 
 import icecube.daq.payload.IHitData;
 import icecube.daq.payload.IByteBufferCache;
+import icecube.daq.payload.IWriteablePayload;
 import icecube.daq.payload.IHitPayload;
+import icecube.daq.payload.IUTCTime;
+import icecube.daq.payload.IPayloadDestination;
 import icecube.daq.payload.PayloadRegistry;
+import icecube.daq.payload.test.MockReadoutRequest;
 import icecube.daq.payload.test.LoggingCase;
 import icecube.daq.payload.test.MockHitData;
 import icecube.daq.payload.test.MockSourceID;
 import icecube.daq.payload.test.MockUTCTime;
+import icecube.daq.payload.test.MockHit;
 import icecube.daq.payload.test.TestUtil;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
+import java.io.IOException;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import junit.textui.TestRunner;
+
+class FooWriteablePayload
+    implements IWriteablePayload
+{
+    public FooWriteablePayload()
+    {
+    }
+    public void dispose()
+    {
+        throw new Error("Unimplemented");
+    }
+    public void recycle()
+    {
+        throw new Error("Unimplemented");
+    }
+    public int writePayload(boolean writeLoaded, IPayloadDestination pDest)
+         throws IOException
+    {
+	return 1; 	
+    }
+    public int writePayload(boolean writeLoaded, int destOffset, ByteBuffer buf)
+         throws IOException
+    {
+        return 1;
+    }
+    public void setCache(IByteBufferCache cache)
+    {
+        throw new Error("Unimplemented");
+    }
+    public int getPayloadInterfaceType()
+    {
+        throw new Error("Unimplemented");
+    }
+    public IUTCTime getPayloadTimeUTC()
+    {
+        throw new Error("Unimplemented");
+    }
+    public int getPayloadType()
+    {
+        throw new Error("Unimplemented");
+    }
+    public int getPayloadLength()
+    {
+        return 1;
+    }
+    public ByteBuffer getPayloadBacking()
+    {
+        throw new Error("Unimplemented");
+    }
+    public Object deepCopy()
+    {
+        throw new Error("Unimplemented");
+    }
+}
 
 class FooCache
     implements IByteBufferCache
@@ -33,7 +93,7 @@ class FooCache
 
     public void destinationClosed()
     {
-        throw new Error("Unimplemented");
+        // do nothing
     }
 
     public void flush()
@@ -93,7 +153,7 @@ class FooCache
 
     public void receiveByteBuffer(ByteBuffer x0)
     {
-        throw new Error("Unimplemented");
+        // do nothing
     }
 
     public void returnBuffer(ByteBuffer x0)
@@ -105,10 +165,8 @@ class FooCache
     {
         // do nothing
     }
-    
-
 }
-public class ReadoutDataFactoryTest
+public class ByteBufferPayloadDestinationTest
     extends LoggingCase
 {
     /**
@@ -116,19 +174,19 @@ public class ReadoutDataFactoryTest
      *
      * @param name the name of the test.
      */
-    public ReadoutDataFactoryTest(String name)
+    public ByteBufferPayloadDestinationTest(String name)
     {
         super(name);
     }
 
     public static Test suite()
     {
-        return new TestSuite(ReadoutDataFactoryTest.class);
+        return new TestSuite(ByteBufferPayloadDestinationTest.class);
     }
 
     public void testCreate()
         throws Exception
-    {
+    {   
         final int uid = 12;
         final int payNum = 1;
         final boolean isLast = true;
@@ -159,16 +217,42 @@ public class ReadoutDataFactoryTest
         ByteBuffer buf =
             TestUtil.createReadoutDataPayload(uid, payNum, isLast, srcId,
                                               firstTime, lastTime, hitList);
-	ReadoutDataFactory rdf = new ReadoutDataFactory(new FooCache());
+	FooCache foo = new FooCache();
+	ByteBuffer buf1 =foo.acquireBuffer(16); 
+	ByteBufferPayloadDestination bpd = new ByteBufferPayloadDestination(foo,new FooCache());
 	try{
-	assertNotNull("Creating payload", rdf.createPayload(buf, -1));
+	ByteBufferPayloadDestination bpd1 = new ByteBufferPayloadDestination(new FooCache(),null);
 	}catch(Error err){
-	if(!err.getMessage().equals("Cannot create readout data")){
+	if(!err.getMessage().equals("Buffer cache is null")){
 	throw err;	
 	}
 	}
-	assertNotNull("Creating payload", rdf.createPayload(buf, 0));
-	assertNotNull("Spliceable returned", rdf.createSpliceable(buf));
+	try{
+	bpd.notifyByteBufferReceiver(buf);
+	}catch(Error err){
+	if(!err.getMessage().equals("Unimplemented")){
+	throw err;	
+	}
+	}
+
+	
+	bpd.recycleByteBuffer(buf);
+	bpd.close();
+	try{
+	assertEquals("writePayload", bpd.writePayload(new FooWriteablePayload()));
+	}catch(IOException ioe){
+	if(!ioe.getMessage().equals("This PayloadDestination is not valid")){
+	throw ioe;
+	}
+	}
+	try{
+	assertEquals("writePayload", bpd.writePayload(true, new FooWriteablePayload()));
+	}catch(IOException ioe){
+	if(!ioe.getMessage().equals("This PayloadDestination is not valid")){
+	throw ioe;
+	}
+	}
+	
 
     }
 
--- src/test/java/icecube/daq/payload/impl/TriggerRequestTest.java
+++ src/test/java/icecube/daq/payload/impl/TriggerRequestTest.java
@@ -18,6 +18,7 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
