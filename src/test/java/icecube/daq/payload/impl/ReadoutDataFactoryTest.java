6e09fb50af4b890768910bcacb9896ca4f80f08d
IceCube Intern <intern@icecube.wisc.edu>
2011-04-01 08:49:10 -0500

    ReadoutDataFactoryTest.java added

---
+++ src/test/java/icecube/daq/payload/impl/ReadoutDataFactoryTest.java
@@ -0,0 +1,179 @@
+package icecube.daq.payload.impl;
+
+import icecube.daq.payload.IHitData;
+import icecube.daq.payload.IByteBufferCache;
+import icecube.daq.payload.IHitPayload;
+import icecube.daq.payload.PayloadRegistry;
+import icecube.daq.payload.test.LoggingCase;
+import icecube.daq.payload.test.MockHitData;
+import icecube.daq.payload.test.MockSourceID;
+import icecube.daq.payload.test.MockUTCTime;
+import icecube.daq.payload.test.TestUtil;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+class FooCache
+    implements IByteBufferCache
+{
+    public FooCache()
+    {
+    }
+
+    public ByteBuffer acquireBuffer(int len)
+    {
+        return ByteBuffer.allocate(len);
+    }
+
+    public void destinationClosed()
+    {
+        throw new Error("Unimplemented");
+    }
+
+    public void flush()
+    {
+        throw new Error("Unimplemented");
+    }
+
+    public int getCurrentAquiredBuffers()
+    {
+        throw new Error("Unimplemented");
+    }
+
+    public long getCurrentAquiredBytes()
+    {
+        throw new Error("Unimplemented");
+    }
+
+    public boolean getIsCacheBounded()
+    {
+        throw new Error("Unimplemented");
+    }
+
+    public long getMaxAquiredBytes()
+    {
+        throw new Error("Unimplemented");
+    }
+
+    public String getName()
+    {
+        throw new Error("Unimplemented");
+    }
+
+    public int getTotalBuffersAcquired()
+    {
+        throw new Error("Unimplemented");
+    }
+
+    public int getTotalBuffersCreated()
+    {
+        throw new Error("Unimplemented");
+    }
+
+    public int getTotalBuffersReturned()
+    {
+        throw new Error("Unimplemented");
+    }
+
+    public long getTotalBytesInCache()
+    {
+        throw new Error("Unimplemented");
+    }
+
+    public boolean isBalanced()
+    {
+        throw new Error("Unimplemented");
+    }
+
+    public void receiveByteBuffer(ByteBuffer x0)
+    {
+        throw new Error("Unimplemented");
+    }
+
+    public void returnBuffer(ByteBuffer x0)
+    {
+        // do nothing
+    }
+
+    public void returnBuffer(int x0)
+    {
+        // do nothing
+    }
+}
+public class ReadoutDataFactoryTest
+    extends LoggingCase
+{
+    /**
+     * Constructs an instance of this test.
+     *
+     * @param name the name of the test.
+     */
+    public ReadoutDataFactoryTest(String name)
+    {
+        super(name);
+    }
+
+    public static Test suite()
+    {
+        return new TestSuite(ReadoutDataFactoryTest.class);
+    }
+
+    public void testCreate()
+        throws Exception
+    {
+        final int uid = 12;
+        final int payNum = 1;
+        final boolean isLast = true;
+        final int srcId = 34;
+        final long firstTime = 1111L;
+        final long lastTime = 2222L;
+
+        final long hitTime1 = 1122L;
+        final int hitType1 = 23;
+        final int hitCfgId1 = 24;
+        final int hitSrcId1 = 25;
+        final long hitDomId1 = 1126L;
+        final int hitMode1 = 27;
+
+        final long hitTime2 = 2211;
+        final int hitType2 = 33;
+        final int hitCfgId2 = 34;
+        final int hitSrcId2 = 35;
+        final long hitDomId2 = 2109L;
+        final int hitMode2 = 37;
+
+        ArrayList hitList = new ArrayList();
+        hitList.add(new MockHitData(hitTime1, hitType1, hitCfgId1, hitSrcId1,
+                                    hitDomId1, hitMode1));
+        hitList.add(new MockHitData(hitTime2, hitType2, hitCfgId2, hitSrcId2,
+                                    hitDomId2, hitMode2));
+	
+        ByteBuffer buf =
+            TestUtil.createReadoutDataPayload(uid, payNum, isLast, srcId,
+                                              firstTime, lastTime, hitList);
+	ReadoutDataFactory rdf = new ReadoutDataFactory(new FooCache());
+	try{
+	assertNotNull("Creating payload", rdf.createPayload(buf, -1));
+	}catch(Error err){
+	if(!err.getMessage().equals("Cannot create readout data")){
+	throw err;	
+	}
+	}
+	assertNotNull("Creating payload", rdf.createPayload(buf, 0));
+	assertNotNull("Spliceable returned", rdf.createSpliceable(buf));
+
+    }
+
+    
+
+    public static void main(String[] args)
+    {
+        TestRunner.run(suite());
+    }
+}
