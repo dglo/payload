708b19d533b60e810575f518588e3a53ca633849
Dave Glowacki <dglo@icecube.wisc.edu>
2013-01-21 04:23:40 -0600

    Issue 6266: Add missing interface types

--- src/main/java/icecube/daq/payload/PayloadInterfaceRegistry.java
+++ src/main/java/icecube/daq/payload/PayloadInterfaceRegistry.java
@@ -5,5 +5,7 @@ package icecube.daq.payload;
  * interfaces by interface type.
  */
 public final class PayloadInterfaceRegistry {
+    public static final int I_HIT_PAYLOAD = 3;      // IHitPayload
+    public static final int I_HIT_DATA_PAYLOAD = 4; // IHitDataPayload
     public static final int I_TRIGGER_REQUEST =  6; // ITriggerRequestPayload
 }
